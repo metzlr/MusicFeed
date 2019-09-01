33c884ce985b324804868df45c7ba2d766b734fa
Reed Metzler-Gilbertz <reedmg15@gmail.com>
2019-08-31 22:42:07 -0400

    Initial Commit

---
+++ MusicFeed/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MusicFeed
+//
+//  Created by Reed Metzler-Gilbertz on 8/31/19.
+//  Copyright © 2019 Reed Metzler-Gilbertz. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MusicFeedTests/MusicFeedTests.swift
@@ -0,0 +1,34 @@
+//
+//  MusicFeedTests.swift
+//  MusicFeedTests
+//
+//  Created by Reed Metzler-Gilbertz on 8/31/19.
+//  Copyright © 2019 Reed Metzler-Gilbertz. All rights reserved.
+//
+
+import XCTest
+@testable import MusicFeed
+
+class MusicFeedTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
