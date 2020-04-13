1859ca45e5d7631a6b02eaa5f88b68583762479a
metzlr <reedmg15@gmail.com>
2020-04-12 17:44:03 -0700

    Added ability to search for artists by entering spotify URL or URI.

--- musicfeed/feed/spotify.py
+++ musicfeed/feed/spotify.py
@@ -1,12 +1,21 @@
 import spotipy
+from spotipy.client import SpotifyException
 from spotipy.oauth2 import SpotifyClientCredentials
 
 sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 
 def artist_search(text):
     q = text+'*'
-    results = sp.search(q, type='artist')
-    artists = results['artists']['items']
+    search_results = sp.search(q, type='artist')
+    artists = []
+    # Do artist lookup in case text is a valid spotify URI or URL
+    try:
+        id_results = sp.artist(text)
+        artists.append(id_results)
+    except SpotifyException:
+        print('Search text wasn\'t an ID')
+
+    artists += search_results['artists']['items']
     print(artists)
     return artists
 
