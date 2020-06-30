5ebd30b25383fcc96f35b9f904ed69f9eb4db9c6
metzlr <reedmg15@gmail.com>
2020-06-30 21:10:59 +0000

    Actually fixed the issue with spotify token refresh

--- feed/spotify.py
+++ feed/spotify.py
@@ -19,7 +19,7 @@ else:
 
 SPOTIPY_CLIENT_ID = config['SPOTIPY_CLIENT_ID']
 SPOTIPY_CLIENT_SECRET = config['SPOTIPY_CLIENT_SECRET']
-#SPOTIPY_REDIRECT_URI = config['SPOTIPY_REDIRECT_URI']
+SPOTIPY_REDIRECT_URI = 'https://musicfeed.info' #config['SPOTIPY_REDIRECT_URI']
 
 sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET))
 
