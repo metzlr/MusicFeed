bc185a7204a49d744c4b4c0fd22000df5aa7a3a7
Reed <reedmg15@gmail.com>
2020-12-27 01:19:45 -0800

    Added base heroku url to allowed hosts

--- musicfeed/settings.py
+++ musicfeed/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['musicfeed.info', '157.245.237.226']
+ALLOWED_HOSTS = ['musicfeed.info', '157.245.237.226', 'musicfeed-django.herokuapp.com']
 
 
 # Application definition
